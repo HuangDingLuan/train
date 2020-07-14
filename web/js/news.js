8b05b236c13e82543331f5f1d4f6e57c9a4bee11
huangdingluan <316943766@qq.com>
2020-07-14 12:42:08 +0800

    3

--- web/js/news.js
+++ web/js/news.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
     function get(page) {
         $('#loading').css('display', 'block');
-        axios.get('https://mock-api.com/Zn5Moxnj.mock/page=' + page)
+        axios.get('https://mock-api.com/Zn5Moxnj.mock/new?page=' + page)
             .then(function (res) {
                 console.log("res", res.data);
                 var i;
@@ -12,12 +12,7 @@ $(document).ready(function () {
                 for (i = 0; i < res.data.length; ++i) {
                     item = res.data[i];
                     console.log('item', item);
-                    $('#introduce').append('<tr>' +
-                        '<td> ' + item.id + '  </td>' +
-                        '<td style="padding:30px;">' + item.name + '</td>' +
-                        '<td style="padding:30px;">' + item.sonority + '</td>' +
-                        '<td style="padding:30px;"> <a href="' + item.url + '"><button class="btn btn-primary">了解更多</button></a> </td>' +
-                        '</tr>');
+                    $('#introduce').append(" <div class='space' > <div class='card' style='width:400px'><img class='card-img-top' src=" + item.src + " alt='Card image' style='width:100%'><div  class='card-body'><h4 class='card-title'>" + item.title + "</h4><p class='card-text'> 发布日期：" + item.date + "<br>" + item.text + "</p><a href='" + item.url + "'class='btn btn-primary'>了解更多</a></div></div></div>");
                 }
                 $('#loading').css('display', 'none');
 
